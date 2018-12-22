1a946b1b924941a1c2271bd1f641d7e3fb24443e
daishi <daishi@axlight.com>
2018-12-22 17:27:27 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # apollo-server-patch-playground-example
 An example to patch GraphQL Playground with Apollo Server
+
+## Background
+
+Apollo server provides Playground without any configuration.
+Playground accepts some options to congfigure its theme or something.
+
+Now, I want to run a script automatically in the browser.
+Specifically, I need an authorization token to put in headers.
+
+This example shows how to inject a client script with apollo-server-express.
+
+Please note that this is a workaround and any better solution 
+would be desired. Use this workaround at your own risk.
+
+## How to run
+
+```bash
+git clone https://github.com/dai-shi/apollo-server-patch-playground-example.git
+cd apollo-server-patch-playground-example
+npm install
+npm start
+```
+
+Then, open http://localhost:3000/graphql
---
+++ src/server.js
@@ -0,0 +1,50 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+const interceptor = require('express-interceptor');
+
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const SCRIPT_TO_INJECT = `
+  <script>
+    window.addEventListener('load', () => {
+      console.log('script injected');
+      const store = window.s;
+      setTimeout(() => {
+        const headers = '{"Authorization": "Bearer token"}';
+        store.dispatch({ type: 'EDIT_HEADERS', payload: { headers } });
+      }, 2000);
+    });
+  </script>
+`;
+const END_BODY = '\n  </body>\n  </html>';
+app.use(interceptor((req, res) => ({
+  isInterceptable: () => /text\/html/.test(res.get('content-type')),
+  intercept: (body, send) => {
+    send(body.replace(END_BODY, SCRIPT_TO_INJECT + END_BODY));
+  },
+})));
+
+server.applyMiddleware({ app });
+
+app.listen(port, () => {
+  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+});
